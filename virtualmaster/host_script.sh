129e178cc6b78c4e5495df6d6d5ff9a0142797d2
Petr Dlouhý <petr.dlouhy@email.cz>
2013-02-02 08:42:00 +0100

    přidání dalších měst z DPNK

--- virtualmaster/host_script.sh
+++ virtualmaster/host_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install imagemagick
+apt-get -y install imagemagick
 
 echo '
 function render {
@@ -33,35 +33,63 @@ cd
 
 export osm_url="http://download.geofabrik.de/openstreetmap/europe/czech_republic.osm.pbf"
 export osm_filename='czech_republic.pbf'
-osm2pgsql -s -d gisczech "/home/mtbmap/$osm_filename" -S "/home/mtbmap/rendering-PNK-ZM/Data/mtbmap.style" -C 2000 -U mtbmap
 
 wget -nv "$osm_url" -O $osm_filename
+osm2pgsql -s -d gisczech "/home/mtbmap/$osm_filename" -S "/home/mtbmap/rendering-PNK-ZM/Data/mtbmap.style" -C 2000 -U mtbmap
 
-grep \<remark\> czech_republic.osm && echo "Downloaded file is not complete" && exit
+grep \<remark\> $osm_filename && echo "Downloaded file is not complete" && exit
 
 cd ~/rendering-PNK-ZM/Devel/systemdeploy/db_scripts/
 ./db_scripts.sh
 
-#CR
-render tiles_PNK "51.1 12 48.5 19" 8 13 "../../Devel/mapnik/my_styles/MTB-main.xml" 
 
-#Středočeský kraj
-render tiles_PNK "50.6228 13.384 49.498 15.55" 8 15 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+#CR
+#render tiles_PNK "51.1 12 48.5 19" 8 13 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+
+#Stredocesky kraj
+#render tiles_PNK "50.6228 13.384 49.498 15.55" 8 15 "../../Devel/mapnik/my_styles/MTB-main.xml" 
 
 #Vetsi Praha:
-render tiles_PNK "50.318 14.018 49.762 14.897" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+#render tiles_PNK "50.318 14.018 49.762 14.897" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
 #render tiles_ZM "50.318 14.018 49.762 14.897" 8 18 "../../Devel/mapnik/my_styles/ZM/osm.xml"
 
 #Mensi Praha:
-#render tiles_PNK "50.17 14.20 49.96 14.65" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
-#render tiles_ZM "50.17 14.20 49.96 14.65" 8 18 "../../Devel/mapnik/my_styles/ZM/osm.xml"
+render tiles_PNK "50.17 14.20 49.96 14.65" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+render tiles_ZM "50.17 14.20 49.96 14.65" 8 18 "../../Devel/mapnik/my_styles/ZM/osm.xml"
+
+#Ostrava
+render tiles_PNK "49.8776 18.1364 49.7615 18.3427" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
 
 #Brno:
 render tiles_PNK "49.3 16.45 49.1 16.8" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
 #render tiles_ZM "49.3 16.45 49.1 16.8" 8 18 "../../Devel/mapnik/my_styles/ZM/osm.xml"
 
-#Děčín
+#Decin
 render tiles_PNK "50.7962 14.1549 50.7254 14.2647" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+
+#Hradec Kralove
+render tiles_PNK "50.2349 15.7841 50.1709 15.8818 " 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+
+#Pardubice
+render tiles_PNK "50.068 15.726 50.004 15.824" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+
+#Liberec
+render tiles_PNK "50.7964 15.0097 50.7323 15.1074" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+
+#Plzen
+render tiles_PNK "49.7857 13.3106 49.7167 13.4198" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+
+#Usti nad Labem
+render tiles_PNK "50.6904 13.9839 50.6463 14.0891" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+
+#Jihlava
+render tiles_PNK "49.4239 15.5491 49.3792 15.6227" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+
+#Jablonec nad Nisou
+render tiles_PNK "50.7506 15.1435 50.713 15.1991" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
+
+#Uherske Hradiste
+render tiles_PNK "49.0933 17.4101 49.0536 17.4976" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" 
 
 export render_BW=false
 if $render_BW; then
