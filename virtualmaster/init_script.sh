9631d16dcd0ccac5a114ae62943b81a0aecbc24b
Petr Dlouhý <petr.dlouhy@email.cz>
2013-09-22 10:18:37 +0200

    virtualmaster script: ssh authorization

---
+++ virtualmaster/init_script.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp /home/automat/.ssh/authorized_keys /home/mtbmap/.ssh/authorized_keys
+chown mtbmap /home/mtbmap/.ssh/authorized_keys
--- virtualmaster/start_rendering.sh
+++ virtualmaster/start_rendering.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-virtualmaster create rendernow --image 3290 --profile small --level testing
+virtualmaster create rendernow --image 3290 --profile small --level testing --script ./init_script.sh
 export SERVER_IP=`virtualmaster list | grep rendernow | cut -f4 -d"|" | tr -d " "`
-scp host_script.sh mtbmap@$SERVER_IP:/home/mtbmap
+scp -o 'HostKeyAlias rendernow' host_script.sh mtbmap@$SERVER_IP:/home/mtbmap 
 ssh mtbmap@$SERVER_IP screen -L -d -m ./host_script.sh
 ssh mtbmap@$SERVER_IP "tail -f screenlog.0"
