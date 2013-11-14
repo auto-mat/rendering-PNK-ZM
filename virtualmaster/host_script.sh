d6537a7e8621014d609213d41a1da95a395d3094
Petr Dlouhý <petr.dlouhy@email.cz>
2013-11-14 11:14:45 +0100

    remove unnecessary blank lines

--- virtualmaster/host_script.sh
+++ virtualmaster/host_script.sh
@@ -81,29 +81,17 @@ render tiles_PNK "51.1 12 48.5 19" 8 14 "../../Devel/mapnik/my_styles/MTB-main.x
 #render tiles_PNK "50.318 14.018 49.762 14.897" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Vetsi Praha"
 
 render tiles_PNK "49.8776 18.1364 49.7615 18.3427" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Ostrava"
-
 render tiles_PNK "49.6204 17.2013 49.5658 17.3098" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Olomouc"
-
 render tiles_PNK "49.3 16.45 49.1 16.8" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Brno"
-
 render tiles_PNK "50.7962 14.1549 50.7254 14.2647" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Decin"
-
 render tiles_PNK "50.2349 15.7841 50.1709 15.8818 " 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Hradec Kralove"
-
 render tiles_PNK "50.068 15.726 50.004 15.824" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Pardubice"
-
 render tiles_PNK "50.7964 15.0097 50.7323 15.1074" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Liberec"
-
 render tiles_PNK "49.7857 13.3106 49.7167 13.4198" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Plzen"
-
 render tiles_PNK "50.6904 13.9839 50.6463 14.0891" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Usti nad Labem"
-
 render tiles_PNK "49.4239 15.5491 49.3792 15.6227" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Jihlava"
-
 render tiles_PNK "50.7506 15.1435 50.713 15.1991" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Jablonec nad Nisou"
-
 render tiles_PNK "49.0933 17.4101 49.0536 17.4976" 8 18 "../../Devel/mapnik/my_styles/MTB-main.xml" "Uherske Hradiste"
-
 
 df -h
 date
