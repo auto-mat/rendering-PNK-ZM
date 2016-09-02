24083f9f2d0f626ab7490c0b9e3012a9fa972ad6
Petr Dlouhý <petr.dlouhy@email.cz>
2016-09-02 11:24:43 +0200

    fix generate scripts

--- Devel/generate_tiles/generate_czech.sh
+++ Devel/generate_tiles/generate_czech.sh
@@ -1,1 +1,2 @@
-./generate_tiles_agregated.py "../../Devel/mapnik/my_styles/MTB-main2.xml" tiles/ 51.13 11.80 48.50 19.00 8 18 4
+#!/bin/sh
+./generate_tiles_agregated.py "../../Devel/mapnik/my_styles/MTB-main.xml" tiles/ 51.13 11.80 48.50 19.00 8 18 4
--- Devel/generate_tiles/generate_prague.sh
+++ Devel/generate_tiles/generate_prague.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 ./generate_tiles_agregated.py "../../Devel/mapnik/my_styles/MTB-main.xml" tiles-`date +%y%m%d`/ 50.318 14.018 49.762 14.897 8 18 4
 rm tiles
 ln -s tiles-`date +%y%m%d` tiles
