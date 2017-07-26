978d6cf856e820fc33308551e5edf58c3bc238cd
Petr Dlouhý <petr.dlouhy@email.cz>
2017-07-26 08:57:33 +0000

    updatemap.sh: improve changing databases

--- Devel/systemdeploy/updatemap.sh
+++ Devel/systemdeploy/updatemap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 ./updatemap.py
 docker rm mtbmap -f
-sudo su postgres -c "dropdb gis"
-sudo su postgres -c "createdb gis -T gis_loading"
+psql -U gis -c "ALTER DATABASE gis_loading RENAME TO gis_loading1;"
+psql gis_loading1 -U gis -c "ALTER DATABASE gis RENAME TO gis_loading;"
+psql gis_loading -U gis -c "ALTER DATABASE gis_loading1 RENAME TO gis;"
 docker run --name mtbmap \
     --add-host=database:127.0.0.1 \
     -v /data/mod_tile:/var/lib/mod_tile \
