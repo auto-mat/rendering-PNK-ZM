9175065f8cad44e71aaea20775cf04afb61bdbdc
Petr Dlouhý <petr.dlouhy@email.cz>
2017-07-26 08:57:56 +0000

    render_map.sh: render Prague first

--- docker/render_map.sh
+++ docker/render_map.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /home/rendering/docker/
-perl render_list_geo.pl -n 2 -m default -z 6 -Z 16 -x 12.0 -X 19.0 -Y 51.1 -y 48.5 #CR
 perl render_list_geo.pl -n 2 -m default -z 6 -Z 18 -Y 50.2131 -x 14.1965 -y 49.918 -X 14.7685 #Praha
+perl render_list_geo.pl -n 2 -m default -z 6 -Z 16 -x 12.0 -X 19.0 -Y 51.1 -y 48.5 #CR
