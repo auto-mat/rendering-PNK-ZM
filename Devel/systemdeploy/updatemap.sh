9aa56e2e387d10ad9f323d7f19d6fdf3c77841d3
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-10-21 21:14:06 +0200

    Devel/systemdeploy/updatemap.sh: remove unnnecessary line code

--- Devel/systemdeploy/updatemap.sh
+++ Devel/systemdeploy/updatemap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 ./updatemap.py
-docker rm mtbmap -f
 psql -U gis -c "ALTER DATABASE gis_loading RENAME TO gis_loading1;"
 psql gis_loading1 -U gis -c "ALTER DATABASE gis RENAME TO gis_loading;"
 psql gis_loading -U gis -c "ALTER DATABASE gis_loading1 RENAME TO gis;"
