6217206578ab560a14f6bfd2a3457c22fdac93c8
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-10-24 18:21:00 +0200

    Revert "Devel/systemdeploy/updatemap.sh: drop all 'gis', 'gis_loading' DB connections, before renaming them"
    This reverts commit ae96ad4c23811359723929fe7ea74cfe08ba5190.

--- Devel/systemdeploy/updatemap.sh
+++ Devel/systemdeploy/updatemap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 ./updatemap.py
-psql -h $POSTGISDB_HOST -U gis -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='gis' OR datname='gis_loading';"
 psql -h $POSTGISDB_HOST -U gis -c "ALTER DATABASE gis_loading RENAME TO gis_loading1;"
 psql -h $POSTGISDB_HOST -d gis_loading1 -U gis -c "ALTER DATABASE gis RENAME TO gis_loading;"
 psql -h $POSTGISDB_HOST -d gis_loading -U gis -c "ALTER DATABASE gis_loading1 RENAME TO gis;"
