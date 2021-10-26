d1084229ed1ae01f5d2e20f5f36ecebda069ad60
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-10-26 14:31:08 +0200

    Revert "Devel/systemdeploy/updatemap.sh: start 'supervisord' daemon with 'renderd' program, before start rendering list"
    This reverts commit 6b1cfb35647d539a0b42ea441891ddbf7a946159.

--- Devel/systemdeploy/updatemap.sh
+++ Devel/systemdeploy/updatemap.sh
@@ -7,5 +7,4 @@ set -e
 psql -h $POSTGISDB_HOST -U gis -c "ALTER DATABASE gis_loading RENAME TO gis_loading1;"
 psql -h $POSTGISDB_HOST -d gis_loading1 -U gis -c "ALTER DATABASE gis RENAME TO gis_loading;"
 psql -h $POSTGISDB_HOST -d gis_loading -U gis -c "ALTER DATABASE gis_loading1 RENAME TO gis;"
-/usr/bin/supervisord
 $RENDERING_DIR/docker/render_map.sh
