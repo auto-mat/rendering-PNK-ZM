287aab335b6d798de54d5ee3c2f93b6e98537360
Tomas Zigo <tomas.zigo@slovanet.sk>
2022-04-19 16:42:26 +0200

    Devel/mapnik: add shell script for updating Mapnik PostGIS connection params values

---
+++ Devel/mapnik/update_postgis_conn_param.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo -e "<Parameter name=\"type\">postgis</Parameter>\n\
+<Parameter name=\"password\">$POSTGISDB_PASSWORD</Parameter>\n\
+<Parameter name=\"host\">'$POSTGISDB_HOST'</Parameter>\n\
+<Parameter name=\"port\">$POSTGISDB_PORT</Parameter>\n\
+<Parameter name=\"user\">$POSTGISDB_USER</Parameter>\n\
+<Parameter name=\"dbname\">$POSTGISDB_NAME</Parameter>\n\
+<Parameter name=\"estimate_extent\">false</Parameter>\n\
+<Parameter name=\"extent\">-20037508,-19929239,20037508,19929239</Parameter>\n\
+" > inc/datasource-settings.xml.inc
