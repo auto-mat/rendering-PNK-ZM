0fe34a4296a91c550dc6c841b897b32cbdb873fa
Automat <it@auto-mat.cz>
2025-11-11 21:42:58 +0100

    Devel/systemdeploy: add network bandwidth limit condition

--- Devel/systemdeploy/set_network_bandwidth.sh
+++ Devel/systemdeploy/set_network_bandwidth.sh
@@ -1,2 +1,10 @@
-/home/mtbmap/wondershaper/wondershaper -a eno1 -u 8192 -d 8192
+WONDERSHAPER=/home/mtbmap/wondershaper/wondershaper
 
+if [ "$1" == "remove" ]; then
+    echo "Remove limit"
+    ${WONDERSHAPER} -c -a eno1
+else
+    echo "Add limit"
+    ${WONDERSHAPER} -c -a eno1
+    ${WONDERSHAPER} -a eno1 -u 8192 -d 8192
+fi
